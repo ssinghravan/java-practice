1962427a44cf680b2c1a650c5a296c395bc86f82
ssinghravan <ssinghr100@gmil.com>
2026-01-28 13:06:25 +0530

    primefactor

--- primefactor.java
+++ primefactor.java
@@ -4,9 +4,21 @@ public class primefactor {
     public static void main(String[] args) {
          Scanner n = new Scanner(System.in);
             int num = n.nextInt();
+           
         for (int i = 1; i <= num; i++) {
+            int count = 0;
             if (num % i == 0 ) {
-                System.out.print(i + " ");
+                
+                for (int j = 2; j <= i; j++) {
+                if(i%j==0){
+                    count++;
+                }
+               
             }
+             if(count==1){
+                System.out.println(i + " is a prime factor");
+            }
+            
         }
-    }}
+        
+    }}}
